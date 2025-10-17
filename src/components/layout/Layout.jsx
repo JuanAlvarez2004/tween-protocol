af15fe1e2af02ab590dd1e34f38f773eed96df2d
JuanAlvarez2004 <168690900+JuanAlvarez2004@users.noreply.github.com>
2025-10-17 17:08:17 -0500

    feat: add Lenis for smooth scrolling and integrate with GSAP in Layout component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,18 +1,32 @@
 import Header from "./Header";
 import BallCursor from "../common/BallCursor";
 import SEOHead from "../common/SEOHead";
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import Home from "../../sections/Home";
 import About from "../../sections/About";
 import Services from "../../sections/Services";
 import Contact from "../../sections/Contact";
+import { ReactLenis } from 'lenis/react'
+import gsap from "gsap";
 
 export default function Layout() {
   const ballCursorRef = useRef(null)
+  const lenisRef = useRef()
+
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+
+    gsap.ticker.add(update)
+
+    return () => gsap.ticker.remove(update)
+  }, [])
 
   return (
     <>
-      <SEOHead 
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+      <SEOHead
         title="Tween Protocol® | Portfolio & Services - Digital Studio"
         description="Explore our portfolio and comprehensive digital services. From front-end development to interactive animations, UX/UI design, and web performance optimization."
         keywords="portfolio, digital services, front-end development, UX/UI design, interactive animations, GSAP, web development, digital branding, web optimization"
